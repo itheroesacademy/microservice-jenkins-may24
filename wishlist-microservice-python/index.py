4bca39ee248684e9e885ffe849e1e1ad105e6d67
itheroesacademy <rahul.itheroes@gmail.com>
2024-07-19 13:24:51 +0530

    Update index.py 7

--- wishlist-microservice-python/index.py
+++ wishlist-microservice-python/index.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 @app.route('/', methods = ['GET'])
 def hello():
    print('Getting List of WishList Items')
